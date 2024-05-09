4987329dce0a427a387d052c607c86bd33db7509
Valdir-coelho-jr <valdircoelho.jr@gmail.com>
2024-05-09 07:38:11 -0300

    feat: insert typeorm, userEntity, connection with db etc

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,24 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { UserService } from './user.service';
+import { UserEntity } from './interfaces/user.entity';
 
 @Controller('user')
 export class UserController {
 
+  constructor(private readonly userService: UserService) {
+
+  }
+
   @Post()
   async createUser(
     @Body() createUser: CreateUserDto
-  ) {
-      return {
-        ...createUser,
-        password: undefined
-      }
+  ): Promise<UserEntity> {
+      return this.userService.createUser(createUser)
   }
 
+  @Get()
+  async getAllUser(): Promise<UserEntity[]> {
+    return this.userService.getAllUser()
+  }
 }
